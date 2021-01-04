c93fa82be590185712d8028e6622f0cdd02e7ed5
Francesco Zatelli <francesco.zatelli@gmail.com>
2021-01-04 20:01:27 +0100

    Revert "Uploaded result tables for PCQCM"
    This reverts commit 084d57d489fd3b7e753052dca8b91ab1610d3bca.

--- AQCM_CleanCode_IBM_readout/corrected_final_version/run_multiple_backends.sh
+++ AQCM_CleanCode_IBM_readout/corrected_final_version/run_multiple_backends.sh
@@ -4,6 +4,6 @@ python aqcm_v3.py   ibmqx2  &
 python aqcm_v3.py   melbourne   &
 python aqcm_v3.py   vigo    &
 python aqcm_v3.py   ourense &
-python aqcm_v3.py   valencia    &
+#python aqcm_v3.py   valencia    &
 python aqcm_v3.py   athens  &
 python aqcm_v3.py   santiago    &
