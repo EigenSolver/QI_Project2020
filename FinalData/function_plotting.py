3719f4f86edda0044f52919610b8704423328138
Francesco Zatelli <francesco.zatelli@gmail.com>
2021-01-07 17:17:55 +0100

    Add data for eEQCM, updated report

--- FinalData/function_plotting.py
+++ FinalData/function_plotting.py
@@ -7,6 +7,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from matplotlib import colors
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+
 
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
     new_cmap = colors.LinearSegmentedColormap.from_list(
@@ -14,18 +16,31 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
         cmap(np.linspace(minval, maxval, n)))
     return new_cmap
 
-def phase_covariant_fidelity(theta,phi):
-    return (3*(4-np.sqrt(2))+(3*np.sqrt(2)-4)*(np.cos(2*phi)+np.cos(2*theta)*(1-np.cos(2*phi))))/(16*(2-np.sqrt(2)))
 
-#This is for xy equator. needs to be corrected
-def economical_phase_covariant_fidelity(theta,phi):
-    return 1/8*(2*np.cos(theta)-(np.sqrt(2)-1)*np.cos(2*theta)+np.sqrt(2)+5)
+def phase_covariant_fidelity(theta, phi):
+    return (3 * (4 - np.sqrt(2)) + (3 * np.sqrt(2) - 4) * (
+            np.cos(2 * phi) + np.cos(2 * theta) * (1 - np.cos(2 * phi)))) / (16 * (2 - np.sqrt(2)))
 
-theta = np.linspace(0, np.pi, 300)
-phi = np.linspace(0, 2*np.pi, 300)
+
+def phase_covariant_fidelity_rotated(theta, phi):
+    return (5 + np.sqrt(2)) / 8 + (1 - np.sqrt(2)) / 8 * np.cos(2 * theta)
+
+
+# This is for xy equator. needs to be corrected
+def economical_phase_covariant_fidelity_rotated(theta, phi):
+    return 1 / 8 * (2 * np.cos(theta) - (np.sqrt(2) - 1) * np.cos(2 * theta) + np.sqrt(2) + 5)
+
+
+def economical_phase_covariant_fidelity(theta, phi):
+    return 1 / 4 * (2 + np.sqrt(2) - np.sin(theta) * np.sin(phi) - (-1 + np.sqrt(2)) * np.sin(theta) ** 2 * np.sin(
+        phi) ** 2)
+
+
+theta = np.linspace(0.01, np.pi - 0.01, 300)
+phi = np.linspace(0, 2 * np.pi, 300)
 
 theta, phi = np.meshgrid(theta, phi)
-p0_1_ideal = economical_phase_covariant_fidelity(theta, phi)
+p0_1_ideal = phase_covariant_fidelity_rotated(theta, phi)
 
 fig = plt.figure()
 
@@ -35,11 +50,12 @@ ax.tick_params(axis='x', colors='white')
 
 cmap = plt.get_cmap('plasma')
 new_cmap = truncate_colormap(cmap, 0, 0.9)
-sc = ax.scatter(phi-np.pi, theta-np.pi/2, c=p0_1_ideal,lw=0, s=20,alpha=1,cmap=new_cmap)
+#sc = ax.scatter(phi - np.pi, theta - np.pi / 2, c=p0_1_ideal, lw=0, s=3, alpha=1, cmap=new_cmap)
+sc = ax.scatter(phi - np.pi, np.pi / 2-theta, c=p0_1_ideal, lw=0, s=3, alpha=1, cmap=new_cmap)
 
-cbar = plt.colorbar(sc,orientation="horizontal")
+cbar = plt.colorbar(sc, orientation="horizontal")
 cbar.set_label('Fidelity')
 plt.title("Economical Phase Covariant QCM\nIdeal\n")
-plt.savefig("economical_phase_covariant.png", dpi=300)
+plt.savefig("test.png", dpi=300)
 
 plt.show()
--- FinalData/run_multiple_backends.sh
+++ FinalData/run_multiple_backends.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_athens.txt Athens  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_ibmqx2.txt Yorktown  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_melbourne.txt Melbourne  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_ourense.txt Ourense  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_santiago.txt Santiago  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_valencia.txt Valencia  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_vigo.txt Vigo  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_athens.txt Athens  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_ibmqx2.txt Yorktown  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_melbourne.txt Melbourne  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_ourense.txt Ourense  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_santiago.txt Santiago  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_valencia.txt Valencia  &
-python sphere_plotter.py  PhaseCovariant/IBM/FullSphereRerun/results_corrected_vigo.txt Vigo
-python sphere_plotter.py  PhaseCovariant/Starmon/FullSphereRerun/results_starmon.txt Starmon-5
-python sphere_plotter.py  PhaseCovariant/Starmon/FullSphereRerun/results_starmon_corrected.txt Starmon-5
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_simulator.txt Simulator  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_simulator.txt Simulator
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_athens.txt Athens  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_ibmqx2.txt Yorktown  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_melbourne.txt Melbourne  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_ourense.txt Ourense  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_santiago.txt Santiago  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_valencia.txt Valencia  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_vigo.txt Vigo  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_athens.txt Athens  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_ibmqx2.txt Yorktown  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_melbourne.txt Melbourne  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_ourense.txt Ourense  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_santiago.txt Santiago  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_valencia.txt Valencia  &
+python sphere_plotter.py  PhaseCovariant/IBM/FullSphere/results_corrected_vigo.txt Vigo &
+python sphere_plotter.py  PhaseCovariant/Starmon/FullSphere/results_starmon.txt Starmon-5 &
+python sphere_plotter.py  PhaseCovariant/Starmon/FullSphere/results_starmon_corrected.txt Starmon-5 &
+
+python sphere_plotter.py  Economical/Spin2/FullSphere/results_spin2.txt Spin-2 &
+python sphere_plotter.py  Economical/Spin2/FullSphere/results_spin2_corrected.txt Spin-2 &
+
+
+python sphere_plotter.py  Economical/IBM/FullSphere/results_simulator.txt Simulator  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_simulator.txt Simulator &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_athens.txt Athens  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_ibmqx2.txt Yorktown  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_melbourne.txt Melbourne  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_ourense.txt Ourense  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_santiago.txt Santiago  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_valencia.txt Valencia  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_vigo.txt Vigo  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_athens.txt Athens  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_ibmqx2.txt Yorktown  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_melbourne.txt Melbourne  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_ourense.txt Ourense  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_santiago.txt Santiago  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_valencia.txt Valencia  &
+python sphere_plotter.py  Economical/IBM/FullSphere/results_corrected_vigo.txt Vigo &
